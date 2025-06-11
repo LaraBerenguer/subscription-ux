7f9e1d45d0b8b8deeced753298c6591f7cb3ad97
LaraBerenguer <laraberenguer@outlook.com>
2025-06-11 13:47:32 +0200

    Implement email validation in CodeInput. Add UserInfo interface.

---
+++ frontend/src/services/validate-api.ts
@@ -0,0 +1,32 @@
+import { API_URL, END_POINTS } from "../config";
+import type { UserInfo } from "../types/types";
+
+//get code
+export const validateEmail = async (user: UserInfo): Promise<{user_id: string}> => {
+    try {
+        const response = await fetch(`${API_URL}/${END_POINTS.email.validate}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user)
+        });
+
+        const data = await response.json();
+
+        if (response.status === 400) {
+            console.error("Invalid email address");
+            throw new Error(data.error || "Something went wrong");
+        }
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return data;
+
+    } catch (error) {
+        console.error('Error verifying code', error);
+        throw error;
+    };
+};
