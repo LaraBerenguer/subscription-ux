0ef68994af68752cabcbc2cb0728393b42cdee05
LaraBerenguer <laraberenguer@outlook.com>
2025-06-12 22:05:34 +0200

    Refactor routing structure and implement GoBackButton component with styles

---
+++ frontend/src/layout/Layout.tsx
@@ -0,0 +1,23 @@
+import { Outlet, useLocation } from "react-router-dom";
+import GoBackButton from "../components/Button/GoBackButton";
+
+const Layout = () => {
+    const buttonPages: Record<string, "transparent" | "rounded"> = {
+        "/verify": "transparent",
+        "/plans": "rounded"
+    };
+
+    const location = useLocation();
+    const currentlocation = buttonPages[location.pathname];
+
+    return (
+        <main>
+            {currentlocation &&
+                <div className="go-back-button">
+                    <GoBackButton variant={currentlocation} /></div>}
+            <Outlet />
+        </main>
+    );
+};
+
+export default Layout;
---
+++ frontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ConnectPage from '../pages/ConnectPage';
+import VerifyPage from '../pages/VerifyPage';
+import PlansPage from '../pages/PlansPage';
+import SuccessPage from '../pages/SuccessPage';
+import Layout from '../layout/layout';
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<ConnectPage />} />
+          <Route path='/verify' element={<VerifyPage />} />
+          <Route path='/plans' element={<PlansPage />} />
+          <Route path='/success' element={<SuccessPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+};
+
+export default AppRoutes;
