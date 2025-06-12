1b37e67b40565a029be506dd2071a5a328126598
LaraBerenguer <laraberenguer@outlook.com>
2025-06-12 17:58:08 +0200

    Add Terms and Conditions component and update PlansPage styles

--- frontend/src/pages/PlansPage.tsx
+++ frontend/src/pages/PlansPage.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import Button from "../components/Button/Button";
 import PriceCard from "../components/Card/PriceCard";
 import { useProductContext } from "../context/ProductsContext";
+import TermsAndConditions from "../components/Terms/TermsAndConditions";
+import "../styles/plansPage.css";
 
 const PlansPage = () => {
     const { products, selectedPrice, error, setError } = useProductContext();
@@ -23,15 +25,19 @@ const PlansPage = () => {
             <section className="plans-page--cards">
                 {products &&
                     Object.entries(products).map(([key, value]) => (
-                        <PriceCard
-                            key={key}
-                            plan={key}
-                            title={key === "year" ? "Annual" : "Monthly"}
-                            price={value.price}
-                            currency={value.currency}
-                            trialDays={value.trial_days}
-                        />))}
-
+                        <div className={`plans-page--card`}>
+                            <PriceCard
+                                key={key}
+                                plan={key}
+                                title={key === "year" ? "Annual" : "Monthly"}
+                                price={value.price}
+                                currency={value.currency}
+                                trialDays={value.trial_days}
+                            />
+                        </div>
+                    ))}
+            </section>
+            <section className="plans-page--cancel">
                 <span><small>Cancel anytime.</small></span>
             </section>
             <section className="plans-page--button">
@@ -39,7 +45,7 @@ const PlansPage = () => {
                 <Button type="button" onClick={handleStartTrial}>Start my free trial!</Button>
             </section>
             <section className="plans-page--privacy">
-                {/*Terms and conditions component*/}
+                <TermsAndConditions />
             </section>
         </main>
     );
