1ad53698d5af3088e7d5457ba3f55050f8f0e4c3
LaraBerenguer <laraberenguer@outlook.com>
2025-06-14 19:03:19 +0200

    Updat main.tsx Providers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import AppRoutes from './routes/AppRoutes.tsx';
-import { EmailProvider } from './context/email/EmailContext.ts';
-import { ProductProvider } from './context/ProductsContext.tsx';
+import { EmailProvider } from './context/email/EmailProvider.tsx';
+import { ProductProvider } from './context/products/ProductsProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
